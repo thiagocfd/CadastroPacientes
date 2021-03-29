19733a8d4f65ce0964323e7c7eca339ff4a9feca
Thiago Dourado <thiagocfdourado@gmail.com>
2021-03-29 18:49:24 -0300

    Criacao do CRUD inicial de Paciente

---
+++ CadastroPacientes/Models/Paciente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPacientes.Models
+{
+    public class Paciente
+    {
+        public long? PacienteID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Diagnostico { get; set; }
+        public string QueixaPrincipal { get; set; }
+    }
+}
