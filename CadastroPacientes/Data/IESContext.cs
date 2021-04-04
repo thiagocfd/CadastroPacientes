aa041c1cc26767cc369984b70b773dad65b635f3
Thiago Dourado <thiagocfdourado@gmail.com>
2021-04-04 10:30:11 -0300

    Implementação da BD de Pacientes

---
+++ CadastroPacientes/Data/IESContext.cs
@@ -0,0 +1,25 @@
+﻿using CadastroPacientes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPacientes.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=IESCadastroPacientes;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }*/
+
+
+    }
+}
---
+++ CadastroPacientes/Data/IESDbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using CadastroPacientes.Models;
+using System.Linq;
+
+
+namespace CadastroPacientes.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Pacientes.Any())
+            {
+                return;
+            }
+
+            var pacientes = new Paciente[]
+            {
+                new Paciente { Nome="Alexandre" },
+                new Paciente { Nome="Carlos"}
+            };
+
+            foreach (Paciente p in pacientes)
+            {
+                context.Pacientes.Add(p);
+            }
+            context.SaveChanges();
+        }
+    }
+}
